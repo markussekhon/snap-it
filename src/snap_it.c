975e2f5d1aa0733688519b9c906a5ec80ebfb327
Markus Sekhon <markus.sekhon@pm.me>
2024-06-09 11:37:23 -0700

    tried to add functionality where you can select a region you want to screenshot, ran into a securities issue with my method, going to try and find a workaround

--- src/snap_it.c
+++ src/snap_it.c
@@ -17,20 +17,61 @@ void takeScreenshot() {
 
 	int screen = DefaultScreen(display);
 	Window root = RootWindow(display, screen);
+	
 	XWindowAttributes window_attributes;
 	XGetWindowAttributes(display, root, &window_attributes);
 	int width = window_attributes.width;
 	int height = window_attributes.height;
+	
+	Window window = XCreateSimpleWindow(display, root, 0, 0, width, height, 0, 0, 0);
 
-	printf("(X,Y):(%d,%d)\n", width, height);
+	XSelectInput(display, window, KeyPressMask | ButtonPressMask | ButtonReleaseMask);
 
-	XImage *image = XGetImage(display, root, 0, 0, width, height, AllPlanes, ZPixmap);
+	int x1,x2,y1,y2;
+
+	printf("outside penor\n");
+
+	XMapWindow(display, window);
+
+	while(1){
+		XEvent event;
+		
+		printf("inbetween penor\n");
+		XNextEvent(display, &event);
+		printf("unknown location penor\n");
+
+		if(event.type == ButtonPress && event.xbutton.button == Button1){
+			x1 = event.xbutton.x;
+			y1 = event.xbutton.y;
+		}
+
+		else if(event.type == ButtonRelease && event.xbutton.button == Button1){
+			x2 = event.xbutton.x;
+			y2 = event.xbutton.y;
+			break;
+		}
+
+		else if(event.xkey.keycode == 27 || event.xbutton.button == Button3){
+			exit(0);
+		}
+		printf("inside penor\n");
+	}
+
+
+	printf("1(X,Y):(%d,%d)\n", x1, y1);
+	printf("2(X,Y):(%d,%d)\n", x2, y2);
+	
+
+	XImage *image = XGetImage(display, root, x1, y1, x2, y2, AllPlanes, ZPixmap);
 
 	if (image == NULL) {
 		fprintf(stderr, "Unable to capture image.\n");
 		exit(1);
 	}
 
+	printf("image captured\n");
+
+	/*
 	FILE *fp = fopen("screenshot.ppm", "wb");
 	
 	if (fp == NULL) {
@@ -52,6 +93,8 @@ void takeScreenshot() {
 
 	fclose(fp);
 
+	*/
+
 	XDestroyImage(image);
 	XCloseDisplay(display);
 
@@ -61,11 +104,12 @@ void takeScreenshot() {
 }
 
 int main(int argc, char *argv[]) {
-	
+	/*
 	if (argc > 2 || strncmp(argv[1], "snap", 4) != 0){
 		fprintf(stderr, "issues with argument passed\n");
 		return 1;
 	}
+	*/
 
 	takeScreenshot();
 
